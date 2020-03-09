810bda04afd9758b41ddda0fcc7838063f815906
Edgar Morales <edgar.morales@softtek.com>
2020-03-09 14:37:40 -0600

    Project Structure
    Web API's

---
+++ emailservice/Controllers/Shopping.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Shopping.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ShoppingController : ControllerBase
+    {
+
+        [HttpGet]
+        public void Get(){
+            
+        }
+    }
+}
